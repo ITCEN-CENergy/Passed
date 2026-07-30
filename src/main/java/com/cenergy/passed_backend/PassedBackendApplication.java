83c2abd3de4d8c0216a19593b209e02a8415b7f2
minho2618 <minho2618@naver.com>
2026-07-30 12:36:35 +0900

    Init Spring Backend Proejct

---
+++ src/main/java/com/cenergy/passed_backend/PassedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cenergy.passed_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cenergy/passed_backend/PassedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenergy.passed_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

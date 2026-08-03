f05f604759f6aca2b6ee77f769b829e7e89243b5
kangminju <yessuk5499@gmail.com>
2026-08-03 14:19:34 +0900

    feat: CoverLetter 엔티티에 생성날짜 적용

--- passed-backend/src/main/java/com/cenergy/passed_backend/domain/coverletter/domain/CoverLetter.java
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/coverletter/domain/CoverLetter.java
@@ -1,19 +1,17 @@
 package com.cenergy.passed_backend.coverletter.entity;
 
+import com.cenergy.passed_backend.common.entity.CreatedAtEntity;
 import com.cenergy.passed_backend.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-
-import java.time.OffsetDateTime;
 
 @Getter
 @Entity
 @Table(name = "cover_letters")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class CoverLetter {
+public class CoverLetter extends CreatedAtEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +21,4 @@ public class CoverLetter {
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
-    @CreationTimestamp
-    @Column(name = "created", nullable = false, updatable = false)
-    private OffsetDateTime created;
 }

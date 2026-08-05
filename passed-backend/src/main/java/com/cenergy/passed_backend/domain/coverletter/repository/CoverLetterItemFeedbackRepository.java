ba67b604a00175140a2aa62571fe9b2ee7022349
minho2618 <minho2618@naver.com>
2026-08-05 14:15:41 +0900

    COPE-30 자기소개서 관련 리포지토리 추가

---
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/coverletter/repository/CoverLetterItemFeedbackRepository.java
@@ -0,0 +1,7 @@
+package com.cenergy.passed_backend.domain.coverletter.repository;
+
+import com.cenergy.passed_backend.domain.coverletter.entity.CoverLetterItemFeedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoverLetterItemFeedbackRepository extends JpaRepository<CoverLetterItemFeedback, Long> {
+}

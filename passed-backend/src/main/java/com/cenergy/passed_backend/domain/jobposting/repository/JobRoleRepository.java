c99ea831d67c9a160b17ff189680bfda4728b385
kangminju <yessuk5499@gmail.com>
2026-08-06 16:59:55 +0900

    CODE-78 레포지토리 관련 파일 경로 수정

---
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/jobposting/repository/JobRoleRepository.java
@@ -0,0 +1,13 @@
+package com.cenergy.passed_backend.domain.jobposting.repository;
+
+import com.cenergy.passed_backend.domain.jobposting.entity.JobRole;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface JobRoleRepository extends JpaRepository<JobRole, Long> {
+    @EntityGraph(attributePaths = "industry")
+    List<JobRole> findAllByIdIn(Collection<Long> ids);
+}

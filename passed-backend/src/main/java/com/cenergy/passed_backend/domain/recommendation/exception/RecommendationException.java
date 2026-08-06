43502b363725a00e4ffdb93b0686491cebf989a6
kangminju <yessuk5499@gmail.com>
2026-08-06 17:34:07 +0900

    CODE-78 예외처리 폴더 추가, 이동

---
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/recommendation/exception/RecommendationException.java
@@ -0,0 +1,16 @@
+package com.cenergy.passed_backend.domain.recommendation.exception;
+
+import com.cenergy.passed_backend.global.error.ErrorCode;
+
+public class RecommendationException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public RecommendationException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}

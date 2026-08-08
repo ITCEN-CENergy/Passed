7d7d633118e1d85d8066e2e672cd415a1e21d996
kangminju <yessuk5499@gmail.com>
2026-08-08 12:01:13 +0900

    CODE-78 JobPostingRepository 함수 추가

--- passed-backend/src/main/java/com/cenergy/passed_backend/domain/jobposting/repository/JobPostingRepository.java
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/jobposting/repository/JobPostingRepository.java
@@ -2,6 +2,7 @@ package com.cenergy.passed_backend.domain.jobposting.repository;
 
 import com.cenergy.passed_backend.domain.jobposting.entity.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,6 +10,9 @@ import java.util.Collection;
 import java.util.List;
 
 public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
+
+    @EntityGraph(attributePaths = "company")
+    List<JobPosting> findAllByIdIn(Collection<Long> ids);
 
     @Query("""
             select jobPosting.id

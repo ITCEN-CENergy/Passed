2762b68a6aed070489c03ff10bb0f577db641a4a
kangminju <yessuk5499@gmail.com>
2026-08-08 16:49:21 +0900

    CODE-78 공고 추천 레포지토리 함수 추가

--- passed-backend/src/main/java/com/cenergy/passed_backend/domain/recommendation/repository/JobRecommendationRepository.java
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/recommendation/repository/JobRecommendationRepository.java
@@ -3,5 +3,11 @@ package com.cenergy.passed_backend.domain.recommendation.repository;
 import com.cenergy.passed_backend.domain.recommendation.entity.JobRecommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JobRecommendationRepository extends JpaRepository<JobRecommendation, Long> {
+    Optional<JobRecommendation> findFirstByJobPostingIdAndRecommendationRunUserIdOrderByRecommendationRunStartedAtDescIdDesc(
+            Long jobPostingId,
+            Long userId
+    );
 }

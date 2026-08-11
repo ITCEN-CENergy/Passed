6edb40e9e4d649fd2360bffb7a671b7c93e66081
kangminju <yessuk5499@gmail.com>
2026-08-11 17:21:34 +0900

    CODE-105 RecommendationExceptionHandler 추천 관련 조회 에러 추가

--- passed-backend/src/main/java/com/cenergy/passed_backend/domain/recommendation/exception/RecommendationExceptionHandler.java
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/recommendation/exception/RecommendationExceptionHandler.java
@@ -7,13 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(assignableTypes = RecommendationController.class)
 public class RecommendationExceptionHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            HttpMessageNotReadableException.class,
+            BindException.class
+    })
     public ResponseEntity<ErrorResponse> invalidRequest(Exception ignored) {
         return response(
                 HttpStatus.BAD_REQUEST,
@@ -27,6 +32,7 @@ public class RecommendationExceptionHandler {
         HttpStatus status = switch (exception.getErrorCode()) {
             case ErrorCode.RECOMMENDATION_INVALID_REQUEST -> HttpStatus.BAD_REQUEST;
             case ErrorCode.RECOMMENDATION_USER_NOT_FOUND, ErrorCode.RECOMMENDATION_USER_SKILLS_NOT_FOUND -> HttpStatus.NOT_FOUND;
+            case ErrorCode.RECOMMENDATION_RUN_NOT_FOUND, ErrorCode.RECOMMENDATION_RESULT_NOT_FOUND -> HttpStatus.NOT_FOUND;
             case ErrorCode.RECOMMENDATION_ALREADY_PROCESSING -> HttpStatus.CONFLICT;
             case ErrorCode.RECOMMENDATION_POLICY_NOT_FOUND -> HttpStatus.SERVICE_UNAVAILABLE;
             default -> HttpStatus.INTERNAL_SERVER_ERROR;

3ff5d25d0232670bd4c5786d9be93968accb02cf
minho2618 <minho2618@naver.com>
2026-08-14 14:10:05 +0900

    fix: CompanyRepository에 메소드 누락된 것 다시 추가 (COPE-124)

--- passed-backend/src/main/java/com/cenergy/passed_backend/domain/jobposting/repository/CompanyRepository.java
+++ passed-backend/src/main/java/com/cenergy/passed_backend/domain/jobposting/repository/CompanyRepository.java
@@ -2,6 +2,16 @@ package com.cenergy.passed_backend.domain.jobposting.repository;
 
 import com.cenergy.passed_backend.domain.jobposting.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    @Query("select company.id as id, company.companyName as name from Company company order by company.companyName asc")
+    List<CompanyNameView> findAllNames();
+
+    interface CompanyNameView {
+        Long getId();
+        String getName();
+    }
 }
